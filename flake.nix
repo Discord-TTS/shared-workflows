6bd1a1d502b87edeac4d63fe727ced1f8c1f5052
GnomedDev <daisy2005thomas@gmail.com>
2025-12-15 14:30:32 +0000

    Implement Nix flake for shared build code

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  outputs =
+    { ... }:
+    {
+      mkTTSModule =
+        {
+          pkgs,
+          package,
+          extraDevTools ? [ ],
+        }:
+        {
+          nixpkgs = pkgs;
+
+          packages.default = package;
+          devShell = pkgs.mkShell {
+            inputsFrom = [ package ];
+            buildInputs =
+              extraDevTools
+              ++ (with pkgs; [
+                rustfmt
+                clippy
+              ]);
+
+            RUST_SRC_PATH = pkgs.rustPlatform.rustLibSrc;
+          };
+
+          dockerImage = pkgs.dockerTools.buildLayeredImage {
+            name = package.name;
+            tag = "latest-${pkgs.stdenv.system}";
+
+            config.Cmd = pkgs.lib.getExe package;
+          };
+        };
+    };
+}
